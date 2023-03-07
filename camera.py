6894ff5937cf701eccfa4633662b79534092cc79
robin <nowrobin3@gmail.com>
2023-03-07 16:07:05 +0900

    feat : flask 설정 및 Opencv 연결

---
+++ camera.py
@@ -0,0 +1,13 @@
+import cv2
+
+
+class VideoCamera(object):
+    def __init__(self):
+      self.video = cv2.VideoCapture(0)
+    def __del__(self):
+      self.video.release()
+    def get_frame(self):
+      ret, frame = self.video.read()
+      # frame = cv2.flip(frame, 1) flip \
+      ret, jpeg = cv2.imencode('.jpg', frame)
+      return jpeg.tobytes()
