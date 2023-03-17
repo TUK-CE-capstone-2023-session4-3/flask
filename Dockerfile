b810d434dcef180031042573a934bf6508890fcc
robin <nowrobin3@gmail.com>
2023-03-17 18:30:57 +0900

    fix : hotfix #1

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,8 @@ RUN pip3 install flask
 COPY . .
 
 
-RUN pip install --no-cache-dir -r requirement.txt
-
+# RUN pip install --no-cache-dir -r requirement.txt
+RUN pip3 install cv2
 
 EXPOSE 8000
 
--- main.py
+++ main.py
@@ -19,4 +19,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, threaded=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
