c7cf04f6d7ac20cc711cf1ca0603b30ee2d15136
robin <nowrobin3@gmail.com>
2023-06-21 18:49:25 +0900

    trial

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, Response
 from camera import VideoCamera
 from flask_ngrok import run_with_ngrok
+from IPython.display import display, Javascript, Image
+from google.colab.output import eval_js
+import html
+from base64 import b64decode, b64encode
+import cv2
+import numpy as np
+import PIL
+import io
 
 app = Flask(__name__)
 run_with_ngrok(app) 
@@ -14,11 +22,159 @@ def gen(camera):
         yield (b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
             
-@app.route('/video_feed')
+@app.route('/video')
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
-     app.run()
+    app.run()
+
+
+
+
+
+
+
+def jsob_to_image(js_object):
+  # decode base64 image
+  image_bytes = b64decode(js_object.split(',')[1])
+  # convert bytes to numpy array
+  img_array = np.frombuffer(image_bytes, dtype=np.uint8)
+  # convert numpy array into OpenCV BGR 
+  frame = cv2.imdecode(img_array, flags=1)
+
+  return frame
+
+
+
+
+
+def video_stream():
+  js = Javascript('''
+    var video;
+    var div = null;
+    var stream;
+    var captureCanvas;
+    var imgElement;
+    var labelElement;
+    
+    var pendingResolve = null;
+    var shutdown = false;
+    
+    function removeDom() {
+       stream.getVideoTracks()[0].stop();
+       video.remove();
+       div.remove();
+       video = null;
+       div = null;
+       stream = null;
+       imgElement = null;
+       captureCanvas = null;
+       labelElement = null;
+    }
+    
+    function onAnimationFrame() {
+      if (!shutdown) {
+        window.requestAnimationFrame(onAnimationFrame);
+      }
+      if (pendingResolve) {
+        var result = "";
+        if (!shutdown) {
+          captureCanvas.getContext('2d').drawImage(video, 0, 0, 640, 480);
+          result = captureCanvas.toDataURL('image/jpeg', 0.8)
+        }
+        var lp = pendingResolve;
+        pendingResolve = null;
+        lp(result);
+      }
+    }
+    
+    async function createDom() {
+      if (div !== null) {
+        return stream;
+      }
+
+      div = document.createElement('div');
+      div.style.border = '2px solid black';
+      div.style.padding = '3px';
+      div.style.width = '100%';
+      div.style.maxWidth = '600px';
+      document.body.appendChild(div);
+      
+           
+      video = document.createElement('video');
+      video.style.display = 'block';
+      video.width = div.clientWidth - 6;
+      video.setAttribute('playsinline', '');
+      video.onclick = () => { shutdown = true; };
+      stream = await navigator.mediaDevices.getUserMedia(
+          {video: { facingMode: "environment"}});
+      div.appendChild(video);
+
+      imgElement = document.createElement('img');
+      imgElement.style.position = 'absolute';
+      imgElement.style.zIndex = 1;
+      imgElement.onclick = () => { shutdown = true; };
+      div.appendChild(imgElement);
+      
+      const instruction = document.createElement('div');
+      instruction.innerHTML = 
+          '<span style="blue: red; font-weight: bold;">' +
+          'click here to stop the video</span>';
+      div.appendChild(instruction);
+      instruction.onclick = () => { shutdown = true; };
+      
+      video.srcObject = stream;
+      await video.play();
+
+      captureCanvas = document.createElement('canvas');
+      captureCanvas.width = 640; 
+      captureCanvas.height = 480; 
+      window.requestAnimationFrame(onAnimationFrame);
+      
+      return stream;
+    }
+    async function stream_frame() {
+      if (shutdown) {
+        removeDom();
+        shutdown = false;
+        return '';
+      }
+
+      var preCreate = Date.now();
+      stream = await createDom();
+      
+      var preShow = Date.now();
+  
+            
+
+      
+      var preCapture = Date.now();
+      var result = await new Promise(function(resolve, reject) {
+        pendingResolve = resolve;
+      });
+      shutdown = false;
+      
+      return {'create': preShow - preCreate, 
+              'show': preCapture - preShow, 
+              'capture': Date.now() - preCapture,
+              'img': result};
+    }
+    ''')
+
+  display(js)
+  
+def video_frame():
+  data = eval_js('stream_frame()')
+  return data
+
+# Get the webcam stream and forward it to python 
+video_stream()
+
+while True:
+    frame_js = video_frame()
+    if not frame_js:
+        break
+    img = jsob_to_image(frame_js["img"])
